71a375ee3d9a61b46f69509458621cc41445f773
Ivan <Korneychik91@yandex.ru>
2022-11-14 23:47:53 +0300

    HW done

--- HomeWork/examples_66/Program.cs
+++ HomeWork/examples_66/Program.cs
@@ -5,15 +5,15 @@ Console.Write("Введи целое число N: ");
 int usernum2 = int.Parse(Console.ReadLine() ?? "");
 if (usernum1 < usernum2)
 {
-    Console.WriteLine($"сумма натуральных чисел от {usernum1} до {usernum2} равна {PrintNaturals(usernum1, usernum2)}");
+    Console.WriteLine($"сумма натуральных чисел от {usernum1} до {usernum2} равна {sumNaturals(usernum1, usernum2)}");
 }
-else Console.WriteLine("вывод невозможен");
-int PrintNaturals(int start, int end)
+else Console.WriteLine("вывод невозможен, т.к. M должно быть меньше N");
+int sumNaturals(int start, int end)
 {
     if (start == end)
     {
         return start;
     }
-    return (start + PrintNaturals(start + 1, end));
+    return (start + sumNaturals(start + 1, end));
 }
 
